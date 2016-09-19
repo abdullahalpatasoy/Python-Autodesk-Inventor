5ff34194c7f0bbfb8cb9cd79e18a2d85c0fd14bf
Abdullah Alp ATASOY <fatmalpsoftware@gmail.com>
2016-09-19 21:40:26 +0300

    Update Inventor_parca_verisi.py

--- Inventor_parca_verisi.py
+++ Inventor_parca_verisi.py
@@ -1,9 +1,9 @@
 import win32com.client
 
-app=win32com.client.Dispatch("Inventor.Application")
-app.Visible = False
+app=win32com.client.Dispatch("Inventor.Application") #Autodesk Inventor başlatılıyor. (Autodesk Inventor is running)
+app.Visible = False #Autodesk Inventor arka planda çalışmasını istiyoruz. I want to running Autodesk Inventor in backend.
 
-Part_FileName_Orig = raw_input('Dosya adresini girin:')
+Part_FileName_Orig = raw_input('Dosya adresini girin:') #You shall enter the file directory.
 
 PartDoc=app.Documents.Open(Part_FileName_Orig)
 
